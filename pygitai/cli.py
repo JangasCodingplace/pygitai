a5caaa25126ab8cd56a2977cefe54b76b6f80f25
JangasCodingplace <janisgoesser92@gmail.com>
2023-12-17 13:15:25 +0100

    "Add .gitignore templates and pygit setup integration"
    "Added `.gitignore` files to the project directories and updated the CLI script to ensure `.pygitai` configurations are set up on execution.
    
    - Created `.gitignore` files for `.pygitai` and `assets` directories to exclude log and sqlite3 files.
    - Added a new `.pygitaiignore` file in the `assets` directory including lock files along with the log and sqlite3 files.
    - Modified `cli.py` to include a setup function `pygit_setup` which creates a `.pygitai` directory at the top-level if it doesn't exist and populates it with default `.gitignore` contents."

--- pygitai/cli.py
+++ pygitai/cli.py
@@ -1,9 +1,24 @@
 import argparse
 
 from . import cmd
+from .common.config import BASE_DIR
+from .common.git import Git
+
+
+def pygit_setup():
+    toplevel_directory = Git.get_toplevel_directory()
+    pygitai_project_config_dir = toplevel_directory / ".pygitai"
+    pygitai_project_config_dir.mkdir(exist_ok=True)
+    # add .gitignore to pygitai directory if it doesn't exist
+    pygitai_gitignore_file = pygitai_project_config_dir / ".gitignore"
+    if not pygitai_gitignore_file.exists():
+        pygitignore_default_file = BASE_DIR / "assets" / ".gitignore"
+        pygitignore_default_file_contents = pygitignore_default_file.read_text()
+        pygitai_gitignore_file.write_text(pygitignore_default_file_contents)
 
 
 def main():
+    pygit_setup()
     parser = argparse.ArgumentParser(
         description="A small package to optimize some git workflows"
     )
