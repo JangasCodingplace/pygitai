aa9ded9617fc48315c30f3123ca991f1e5888294
JangasCodingplace <janisgoesser92@gmail.com>
2023-12-17 13:18:47 +0100

    "Refactor file pattern matching into git module"
    "This commit introduces utility functions to manage ignore patterns within the `pygitai` project. Specifically, added functions include `get_ignored_file_patterns`, which reads the `.pygitaiignore` file to obtain patterns for files to ignore, and `file_name_matches_patterns`, which checks if a given filename matches any of the specified ignore patterns. The `.pygitaiignore` handling is now done directly within the `git.py` module, as evident by the functions being moved from `utils.py` and their corresponding import removal. This refactoring centralizes ignore pattern processing, streamlining the way `pygitai` manages ignored files."

--- pygitai/common/git.py
+++ pygitai/common/git.py
@@ -1,12 +1,29 @@
+import fnmatch
 import subprocess
 from dataclasses import dataclass
 from pathlib import Path
 
 from .config import config
 from .logger import get_logger
-from .utils import file_name_matches_patterns, get_ignored_file_patterns
 
 logger = get_logger(__name__, config.logger.level)
+
+
+def get_ignored_file_patterns() -> list[str]:
+    top_level_directory = Git.get_toplevel_directory()
+    pygitai_ignore_file = top_level_directory / ".pygitaiignore"
+    if pygitai_ignore_file.exists():
+        with pygitai_ignore_file.open("r") as f:
+            return f.read().splitlines()
+    return []
+
+
+def file_name_matches_patterns(file_name: str, patterns: list[str]) -> bool:
+    """Check if the file name matches any of the patterns"""
+    for pattern in patterns:
+        if fnmatch.fnmatch(file_name, pattern):
+            return True
+    return False
 
 
 class PreCommitHook:
