385419d61354afcacd03c7df1a4bcd5526b26329
JangasCodingplace <janisgoesser92@gmail.com>
2023-12-20 00:45:55 +0100

    Enhance LLM loading with dynamic sys.path customization support

--- pygitai/common/__init__.py
+++ pygitai/common/__init__.py
@@ -1,10 +1,20 @@
+import sys
+
 from . import llm
 from .config import config
 from .git import Git, PreCommitHook
 from .git import state as git_state
 from .logger import get_logger
 
-LLM = getattr(llm, config.general.llm)
+# load custom project scope
+sys.path.append((config.general.toplevel_directory / ".pygitai").as_posix())
+import pygitai_customization  # noqa
+
+try:
+    LLM = getattr(llm, config.general.llm)
+except AttributeError:
+    LLM = getattr(pygitai_customization.llm, config.general.llm)
+
 
 __all__ = [
     "config",
--- pygitai/common/llm/__init__.py
+++ pygitai/common/llm/__init__.py
@@ -1,5 +1,12 @@
+import sys
+
+from pygitai.common.config import config
+
 from .base import LLMBase, ParserBase
 from .openai import OpenAI
+
+sys.path.append((config.general.toplevel_directory / ".pygitai").as_posix())
+from pygitai_customization.llm import *  # noqa
 
 __all__ = [
     "LLMBase",
