c8fa3b28ef0ce0466808123c4610e251a3a3c7cb
JangasCodingplace <janisgoesser92@gmail.com>
2023-12-22 11:29:48 +0100

    Add documentation and update classes for custom job creation

---
+++ docs/api/BaseJob.md
@@ -0,0 +1,1 @@
+::: pygitai.common.jobs.base_job.BaseJob
--- pygitai/common/jobs/base_job.py
+++ pygitai/common/jobs/base_job.py
@@ -3,11 +3,34 @@ from argparse import Namespace
 
 
 class BaseJob(ABC):
-    """Base class for Job"""
+    """Base class for Job.
+
+    A Job will always be executed as part of a pipeline.
+
+    Attributes:
+        cli_configurable_name (str | None): The name of the
+            attribute in the cli args that will be used to
+            determine if this job should be executed. Only boolean
+            values are supperted at the moment. If it is None, the
+            job will always be executed.
+        cli_args (Namespace): The cli arguments.
+        kwargs (dict): The keyword arguments that were passed to the job.
+    """
 
     cli_configurable_name: str | None = None
 
     def perform(self, cli_args: Namespace, *args, **kwargs):
+        """Perform the job. This method is just a wrapper around
+        `exec_command`. It's used to determine if the job should be
+        executed or not.
+
+        Args:
+            cli_args (Namespace): The cli arguments. Those arguments
+                will be bound to the job instance.
+            **kwargs: The keyword arguments that were passed to the
+                job. Those arguments will be bound to the job
+                instance.
+        """
         self.cli_args = cli_args
         self.kwargs = kwargs
 
